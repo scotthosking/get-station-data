84f48821ba80965cf8a3f0e248e2fd64425c7603
Scott Hosking <jshosking@gmail.com>
2019-06-13 19:38:14 +0100

    add london data + processing code

---
+++ Examples/process_data.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+from get_station_data import ghcnm
+
+### Name of original data file from GHCN-M
+### ftp://ftp.ncdc.noaa.gov/pub/data/ghcn/v3/
+
+### Define version number and data + metadata filenames
+data_version = 'v3.3.0.20180404'
+data_fname   = 'raw_data/ghcnm.tavg.'+data_version+'.qca.dat'
+stn_md_fname = 'raw_data/ghcnm.tavg.'+data_version+'.qca.inv'
+
+### Get metadata data for all weather stations around the globe
+stn_md = ghcnm.get_stn_metadata(stn_md_fname)
+
+### Extract metadata for specified countries
+country_names = ['UNITED KINGDOM']
+my_stns = ghcnm.extract_countries(stn_md, country_names)
+
+### Get all available data for all specified stations 
+df = ghcnm.get_data(data_fname, my_stns)
+
+### Save to a CSV file
+df.to_csv('UK_weather_station_data_ghcnm.csv', index=False)
