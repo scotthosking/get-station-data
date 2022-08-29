08ed3afddab9769edb5a078ddd654b56245877d9
Scott Hosking <jshosking@gmail.com>
2022-08-29 17:33:05 +0100

    regid to add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='getGHCN',
+    version='0.01',
+    author='Scott Hosking',
+    author_email='jask@bas.ac.uk',
+    packages=find_packages(),
+    url='https://github.com/scotthosking/get-station-data',
+    description='Package for fetching weather station data from GHCN-Daily and GHCN-monthly',
+    long_description=open('README.md').read(),
+    license='TBD',
+    keywords = "meteorology climatology analysis in_situ",
+    scripts=['getGHCN/ghcnd.py', 'getGHCN/ghcnm.py'],
+    data_files = [("", ["LICENSE.txt"])],
+    install_requires=[
+          'numpy',
+          'pandas',
+          'datetime',
+    ],
+)
